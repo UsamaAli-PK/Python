aa3dba0ab454eaa40408c29d787d51a50ab0961a
UsamaAli-PK <uali24272@gmail.com>
2025-03-13 02:04:54 +0500

    Updated

--- Projects/Windchill/Solution.py
+++ Projects/Windchill/Solution.py
@@ -1,24 +1,27 @@
-# Convert rods to other units and calculate walking time
-# Constants
-METERS_PER_ROD = 5.0292
-FEET_PER_METER = 1 / 0.3048
-MILES_PER_METER = 1 / 1609.34
-FURLONGS_PER_ROD = 1 / 40
-WALKING_SPEED_MPH = 3.1
+def conversion(temp,speed):
+    
+    air_temp = temp
+    air_speed = speed 
+    
+    if air_speed < 0:
+        print("Error: Wind speed cannot be negative")
+        return
+    
+    wct_index = 35.74 + (0.6215*air_temp) - (35.75 * air_speed**0.16) + (0.4275*air_temp*air_speed**0.16)
+    print(
+          f" Temperature (in F) = {air_temp}",
+          f" Air speed (in MPH) = {air_speed}",
+          f" Wind Chill Temperature Index = {wct_index}",
+          sep="\n")
+    print()
 
-def conversion(rod):
-    meters = rod * METERS_PER_ROD
-    feet = meters * FEET_PER_METER
-    miles = meters * MILES_PER_METER
-    furlongs = rod * FURLONGS_PER_ROD
-    time = (miles / WALKING_SPEED_MPH) * 60  
 
-    print(f"You input {rod} rods.",
-          f"Meters: {meters}",
-          f"Feet: {feet}",
-          f"Miles: {miles:.3f}",
-          f"Furlongs: {furlongs}",
-          f"Time: {time:.3f} minutes", sep="\n")
+conversion(10,15)
+conversion(0,25)
+conversion(-10 , 35)
 
-rods = float(input("Enter the number of rods: "))
-conversion(rods)
+Temperature = float(input("Enter Temperature (in F) = "))
+
+Speed = float(input("Enter Air speed (in MPH) = "))
+
+conversion(Temperature,Speed)
